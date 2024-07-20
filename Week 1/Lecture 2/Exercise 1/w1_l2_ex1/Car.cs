71d2a78eea7352e93e1b86d6fae870fe1c250001
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-20 13:35:08 +1200

    W1 L2 EX1 Task 2

---
+++ Week 1/Lecture 2/Exercise 1/w1_l2_ex1/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1_l2_ex1
+{
+    internal class Car: Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+        public string Type { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine("Driving the car...");
+        }
+    }
+}
---
+++ Week 1/Lecture 2/Exercise 1/w1_l2_ex1/Motorcycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1_l2_ex1
+{
+    internal class Motorcycle
+    {
+        public double EngineSize { get; set; }
+        
+        public void Ride()
+        {
+            Console.WriteLine("Writing the motorcycle...");
+        }
+    }
+}
