9f1a669f3078e23d7d08d4ea1a80ee6384227ec4
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-20 13:03:28 +1200

    W1 L1 EX2 Task 1

---
+++ Week 1/Lecture 1/Exercise 2/w1_l1_ex2/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1_l1_ex2
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+    }
+}
