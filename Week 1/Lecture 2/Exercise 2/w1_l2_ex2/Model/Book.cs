1d205df91979fd35b7cf5870a29f8ce391a1aef5
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-20 13:54:15 +1200

    Create Model Folder. (3) and Book Class.

---
+++ Week 1/Lecture 2/Exercise 2/w1_l2_ex2/Model/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1_l2_ex2.Model
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+    }
+}
