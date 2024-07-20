2c978a38b2c86a5e13b847e19aac347375fdade3
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-20 13:56:30 +1200

    Create Interface

---
+++ Week 1/Lecture 2/Exercise 2/w1_l2_ex2/Data/ILibraryRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using w1_l2_ex2.Model;
+
+namespace w1_l2_ex2.Data
+{
+    internal interface ILibraryRepo
+    {
+        public IEnumerable<Book> GetBooks();
+    }
+}
