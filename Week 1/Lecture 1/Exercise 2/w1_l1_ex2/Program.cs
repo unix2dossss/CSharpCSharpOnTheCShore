e4947c035fb15ae5e656ebd78a1bf469ade25187
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-20 13:06:12 +1200

    W1 L1 EX2 Task 2

--- Week 1/Lecture 1/Exercise 2/w1_l1_ex2/Program.cs
+++ Week 1/Lecture 1/Exercise 2/w1_l1_ex2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Student student1 = new Student()
+        {
+            FirstName = "Vandan",
+            LastName = "Bhatt",
+            Age = 19,
+            Gender = "Male",
+            Department = "Computer Science"
+        };
+
+        Student student2 = new Student()
+        {
+            FirstName = "Sai",
+            LastName = "Kiran",
+            Age = 20,
+            Gender = "Male",
+            Department = "Computer Systems"
+        };
+
+        Student student3 = new Student()
+        {
+            FirstName = "Charanjeet",
+            LastName = "Viswanathan",
+            Age = 20,
+            Gender = "Male",
+            Department = "Computer Systems"
+        };
     }
 }
