ddbd20c9bebf296594002ec03dff430fa04abdc1
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-20 13:27:48 +1200

    W1 L2 EX1 Task 1

---
+++ Week 1/Lecture 2/Exercise 1/w1_l2_ex1/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1_l2_ex1
+{
+    internal class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Make: {Make}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Year: {Year}");
+        }
+    }
+}
