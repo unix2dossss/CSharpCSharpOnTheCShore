1deb1c7dea3f731cbf4afc24c481717316a3d79d
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-21 16:50:25 +1200

    Declare Delegate

---
+++ Week 1/Lecture 3/Exercise 2/w1_l3_ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace w1_l3_ex2;
+
+public delegate int Operation(int a, int b);
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+    }
+
+    public int Add(int a, int b)
+    {
+        return a + b; 
+    }
+
+}
