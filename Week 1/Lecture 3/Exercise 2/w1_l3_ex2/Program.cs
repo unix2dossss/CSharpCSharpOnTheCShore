4610c53457a203d2595826360571f100dc24f529
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-21 17:11:19 +1200

    Call Perform Operation

--- Week 1/Lecture 3/Exercise 2/w1_l3_ex2/Program.cs
+++ Week 1/Lecture 3/Exercise 2/w1_l3_ex2/Program.cs
@@ -12,8 +12,18 @@ class Program
         int add1 =  Addition(3, 4);
         int sub1 = Subtraction(4, 3);
 
-        Console.WriteLine(add1);
-        Console.WriteLine(sub1);
+        //Console.WriteLine(add1);
+        //Console.WriteLine(sub1);
+
+        Operation op1;
+        op1 = Addition;
+        Operation op2 = new Operation(Subtraction);
+
+        int out1 = PerformOperation(op1, 3, 2);
+        int out2 = PerformOperation(op2, 3, 2);
+
+        Console.WriteLine($"Performing addition: {out1}");
+        Console.WriteLine($"Performing subtraction: {out2}");
     }
 
     public static int Addition(int a, int b) { return a + b; }
