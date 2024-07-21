ace30934b07e3c989cdbcc520e7117c236f200ce
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-21 16:58:59 +1200

    Add and Subtract

--- Week 1/Lecture 3/Exercise 2/w1_l3_ex2/Program.cs
+++ Week 1/Lecture 3/Exercise 2/w1_l3_ex2/Program.cs
@@ -1,19 +1,22 @@
-﻿namespace w1_l3_ex2;
+﻿using System.Data;
+
+namespace w1_l3_ex2;
 
 public delegate int Operation(int a, int b);
 
 class Program
 {
 
-
     static void Main(string[] args)
     {
+        int add1 =  Addition(3, 4);
+        int sub1 = Subtraction(4, 3);
 
+        Console.WriteLine(add1);
+        Console.WriteLine(sub1);
     }
 
-    public int Add(int a, int b)
-    {
-        return a + b; 
-    }
-
+    public static int Addition(int a, int b) { return a + b; }
+    
+    public static int Subtraction(int a, int b) { return a - b; }
 }
