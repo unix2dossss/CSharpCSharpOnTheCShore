575bad24cec4ed16cbb22b5f28f9ffcc7faf4968
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-22 20:26:44 +1200

    Create Person

---
+++ Week 2/Lecture 1/Exercise 3/w2_l1_ex3/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w2_l1_ex3
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Week 2/Lecture 1/Exercise 3/w2_l1_ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace w2_l1_ex3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
