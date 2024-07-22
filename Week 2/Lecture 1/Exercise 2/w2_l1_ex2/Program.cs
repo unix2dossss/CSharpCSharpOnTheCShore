9470f390cadade059016a37b15bbe409678350ce
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-22 19:34:33 +1200

    Add Employees

--- Week 2/Lecture 1/Exercise 2/w2_l1_ex2/Program.cs
+++ Week 2/Lecture 1/Exercise 2/w2_l1_ex2/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee() {Name="Vandan", Department="D1", Salary=40000},
+            new Employee() {Name="Charan", Department="D2", Salary=55000},
+            new Employee() {Name="Sai", Department="D3", Salary=60000},
+        };
+
+
     }
 }
