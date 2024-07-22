29cfa76283a6e3a565fea4655fd13c638c058e93
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-22 19:32:15 +1200

    Create Employee.cs

---
+++ Week 2/Lecture 1/Exercise 2/w2_l1_ex2/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w2_l1_ex2
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+    }
+}
