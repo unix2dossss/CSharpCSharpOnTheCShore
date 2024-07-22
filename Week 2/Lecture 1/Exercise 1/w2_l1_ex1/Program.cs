9202057956eaa0c2c62318d837f3a963bbdf1f2b
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-22 19:28:40 +1200

    Where LINQ

--- Week 2/Lecture 1/Exercise 1/w2_l1_ex1/Program.cs
+++ Week 2/Lecture 1/Exercise 1/w2_l1_ex1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Person> people = new List<Person>()
+        {
+            new Person(){Name="Vandan", Age=19},
+            new Person(){Name="Krupa", Age=43},
+            new Person(){Name="Snehal", Age=52}
+        };
+
+        IEnumerable<Person> over30 = people.Where<Person>(e => e.Age > 30);
+
+        foreach (Person p in over30)
+        {
+            Console.WriteLine($"Name: {p.Name}, Age: {p.Age} ");
+        }
     }
 }
