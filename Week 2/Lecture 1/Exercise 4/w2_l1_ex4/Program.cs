af489429a3ec952df689f5052cf5507236249652
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-22 20:48:54 +1200

    Sort

--- Week 2/Lecture 1/Exercise 4/w2_l1_ex4/Program.cs
+++ Week 2/Lecture 1/Exercise 4/w2_l1_ex4/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Person> people = new List<Person>() {
+            new Person(){Name="Bob", Age=35},
+            new Person(){Name="Charlie", Age=30},
+            new Person(){Name="Alice", Age=25},
+            new Person(){Name="Eve", Age=30},
+            new Person(){Name="David", Age=20},
+        };
+
+        IEnumerable<Person> ascendingPeople = people.OrderBy(e => e.Age).ThenBy(e => e.Name);
+
+        Console.WriteLine("Sorted list of persons:");
+
+        foreach (Person p in ascendingPeople)
+        {
+            Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
+        }
     }
 }
