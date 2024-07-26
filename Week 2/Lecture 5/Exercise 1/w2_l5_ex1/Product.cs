1b5a4e5d09b35ca792e1204555052f43e653188a
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-26 14:12:59 +1200

    Create Product Class

---
+++ Week 2/Lecture 5/Exercise 1/w2_l5_ex1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace w2_l5_ex1
+{
+    internal class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string? Category { get; set; }
+        public string? Brand { get; set; }
+    }
+}
