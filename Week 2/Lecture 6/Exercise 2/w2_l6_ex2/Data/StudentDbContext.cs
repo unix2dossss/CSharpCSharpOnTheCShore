3a8746ed59f49f18d1331c8209d3129ec5ad2453
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-28 20:10:36 +1200

    Create DbContext

--- Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Data/StudentDbContext.cs
+++ Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Data/StudentDbContext.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using w2_l6_ex2.Model;
 
 namespace w2_l6_ex2.Data
 {
-    internal class StudentDbContext
+    internal class StudentDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=StudentsEx.sqlite");
+        }
+
+        public DbSet<Student> Students { get; set; }
     }
 }
