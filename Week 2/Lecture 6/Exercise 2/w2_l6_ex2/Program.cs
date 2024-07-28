098a7bb296f6b515d689aadd9e27011427c09ad0
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-28 20:25:34 +1200

    Create Student

--- Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Program.cs
+++ Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace w2_l6_ex2;
+﻿using w2_l6_ex2.Data;
+using w2_l6_ex2.Model;
+
+namespace w2_l6_ex2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CreateStudent(new Student { Id = 1, FirstName = "Vandan", LastName = "Bhatt", Address = "Auckland" });
+    }
+
+    public static void CreateStudent(Student student)
+    {
+        using (var dbContext = new StudentDbContext())
+        {
+            var s = dbContext.Students.FirstOrDefault<Student>(s => student.Id == s.Id);
+            if (s == null)
+            {
+                dbContext.Students.Add(student);
+                dbContext.SaveChanges();
+            } else
+            {
+                Console.WriteLine($"Student with ID: {student.Id} already exists");
+            }
+        }
     }
 }
