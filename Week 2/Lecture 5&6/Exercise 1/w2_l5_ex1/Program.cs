7dda94920bd13eff2d1c8097df6ef7936b53c957
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-28 19:29:37 +1200

    Save Changes

--- Week 2/Lecture 5&6/Exercise 1/w2_l5_ex1/Program.cs
+++ Week 2/Lecture 5&6/Exercise 1/w2_l5_ex1/Program.cs
@@ -1,158 +1,68 @@
-﻿//using w2_l5_ex1.Data;
-//using w2_l5_ex1.Model;
-
-//namespace w2_l5_ex1;
-
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        Console.WriteLine("Hello, World!");
-//        CreateProduct(new Product() { ID = 1, Name = "Gaming Mouse", Category = "Electronics", Brand = "Logitech" });
-
-//        //var updated_mouse = new Product() { ID = 1, Name = "Gaming Mouse 2", Category = "Electronics", Brand = "Logitech" };
-
-//        //IEnumerable<Product> products = GetProducts();
-//        //foreach (Product p in products) Console.WriteLine($"{p.ID}, {p.Brand}, {p.Name}, {p.Category}");
-
-//        //UpdateProduct(updated_mouse);
-
-//    }
-
-//    static void CreateProduct(Product product)
-//    {
-//        using (var dbContext = new ProductDbContext())
-//        {
-//            dbContext.Products.Add(product);
-//            dbContext.SaveChanges();
-//        }
-//    }
-
-//    static IEnumerable<Product> GetProducts()
-//    {
-//        using (var dbContext = new ProductDbContext())
-//        {
-//            return dbContext.Products.ToList();
-//        }
-//    }
-
-//    static void UpdateProduct(Product product)
-//    {
-//        using (var dbContext = new ProductDbContext())
-//        {
-//            Product? selected_product = dbContext.Products.FirstOrDefault<Product>(e => e.ID == product.ID);
-//            if (selected_product != null)
-//            {
-//                selected_product.ID = product.ID;
-//                selected_product.Name = product.Name;
-//                selected_product.Brand = product.Brand;
-//                selected_product.Category = product.Category;
-//                dbContext.SaveChanges();
-//            }
-//        }
-//    }
-//}
-
-
-using w2_l5_ex1.Model;
+﻿using Microsoft.EntityFrameworkCore;
 using w2_l5_ex1.Data;
-using System.Xml.Linq;
+using w2_l5_ex1.Model;
 
+namespace w2_l5_ex1;
 
-namespace w2_l5_ex1
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Hello, World!");
+        CreateProduct(new Product() { ID = 1, Name = "Gaming Mouse", Category = "Electronics", Brand = "Logitech" });
+
+        //var updated_mouse = new Product() { ID = 1, Name = "Gaming Mouse 2", Category = "Electronics", Brand = "Logitech" };
+
+        //IEnumerable<Product> products = GetProducts();
+        //foreach (Product p in products) Console.WriteLine($"{p.ID}, {p.Brand}, {p.Name}, {p.Category}");
+
+        //UpdateProduct(updated_mouse);
+
+    }
+
+    static void CreateProduct(Product product)
+    {
+        using (var dbContext = new ProductDbContext())
         {
-
-            CreateProduct(
-                new Product
-                {
-                    ID = 1,
-                    Name = "Iphone 14",
-                    Brand = "Apple",
-                    Category = "Phone"
-                });
-
-
-            //IEnumerable<Product> products = GetProduct();
-            //foreach (Product p in products)
-            //{
-            //    Console.WriteLine($"ID: {p.Id}, Name: {p.Name}");
-            //}
-            //Console.WriteLine();
-
-            //UpdateProduct(
-            //    new Product
-            //    {
-            //        Id = 1,
-            //        Name = "Iphone 12",
-            //        Brand = "Apple",
-            //        Category = "Phone"
-            //    });
-
-            //DeleteProduct(1);
-
-            //IEnumerable<Product> AfterDeleteproducts = GetProduct();
-            //foreach (Product p in AfterDeleteproducts)
-            //{
-            //    Console.WriteLine($"ID: {p.Id}, Name: {p.Name}");
-            //}
+            dbContext.Products.Add(product);
+            dbContext.SaveChanges();
         }
+    }
 
-        private static void CreateProduct(Product p)
+    static IEnumerable<Product> GetProducts()
+    {
+        using (var dbContext = new ProductDbContext())
         {
-            using (ProductDbContext db = new ProductDbContext())
+            return dbContext.Products.ToList();
+        }
+    }
+
+    static void UpdateProduct(Product product)
+    {
+        using (var dbContext = new ProductDbContext())
+        {
+            Product? selected_product = dbContext.Products.FirstOrDefault<Product>(e => e.ID == product.ID);
+            if (selected_product != null)
             {
-                db.Products.Add(p);
-                db.SaveChanges();
+                selected_product.ID = product.ID;
+                selected_product.Name = product.Name;
+                selected_product.Brand = product.Brand;
+                selected_product.Category = product.Category;
+                dbContext.SaveChanges();
             }
         }
+    }
 
-        private static IEnumerable<Product> GetProduct()
-
+    static void DeleteProduct(int ID)
+    {
+        using (var dbContext = new ProductDbContext())
         {
-            List<Product> products;
-            using (ProductDbContext db = new ProductDbContext())
+            var selected_product = dbContext.Products.FirstOrDefault<Product>(e => e.ID == ID);
+            if (selected_product != null)
             {
-                products = db.Products.ToList();
-            }
-            return products;
-        }
-
-        private static void UpdateProduct(Product product)
-
-        {
-            using (ProductDbContext db = new ProductDbContext())
-            {
-                Product? p = db.Products.FirstOrDefault(p => p.ID == product.ID);
-                if (p != null)
-                {
-                    p.Name = "Samsung note5";
-                    p.Brand = "Samsung";
-                    p.Category = "Phone";
-                }
-
-                db.SaveChanges();
+                dbContext.Products.Remove(selected_product);
+                dbContext.SaveChanges();
             }
         }
-
-        private static void DeleteProduct(int id)
-
-        {
-            using (ProductDbContext db = new ProductDbContext())
-            {
-                Product? p = db.Products.FirstOrDefault(p => p.ID == id);
-                if (p != null)
-                {
-                    Console.WriteLine("Removed");
-                    db.Remove(p);
-                    db.SaveChanges();
-                }
-
-            }
-        }
-
     }
 }
