d8971b38a791a69ffc8bf2c4d54ff2f30609cf68
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-28 20:55:26 +1200

    Test Update

--- Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Program.cs
+++ Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Program.cs
@@ -8,8 +8,14 @@ class Program
     static void Main(string[] args)
     {
         CreateStudent(new Student { Id = 1, FirstName = "Vandan", LastName = "Bhatt", Address = "Auckland" });
-
         var students = GetStudents();
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Id: {student.Id}, FirstName: {student.FirstName}, LastName: {student.LastName}, Address: {student.Address}");
+        }
+
+        UpdateStudent(new Student { Id = 1, FirstName = "Vandan", LastName = "Bhatt", Address = "Auckland, Papatoetoe" });
+        students = GetStudents();
         foreach (var student in students)
         {
             Console.WriteLine($"Id: {student.Id}, FirstName: {student.FirstName}, LastName: {student.LastName}, Address: {student.Address}");
@@ -20,15 +26,15 @@ class Program
     {
         using (var dbContext = new StudentDbContext())
         {
-            //var s = dbContext.Students.FirstOrDefault<Student>(s => student.Id == s.Id);
-            //if (s == null)
-            //{
+            var s = dbContext.Students.FirstOrDefault<Student>(s => student.Id == s.Id);
+            if (s == null)
+            {
                 dbContext.Students.Add(student);
                 dbContext.SaveChanges();
-            //} else
-            //{
-            //    Console.WriteLine($"Student with ID: {student.Id} already exists");
-            //}
+            } else
+            {
+                Console.WriteLine($"Student with ID: {student.Id} already exists");
+            }
         }
     }
 
@@ -37,6 +43,21 @@ class Program
         using (var dbContext = new StudentDbContext())
         {
             return dbContext.Students.ToList<Student>();
+        }
+    }
+
+    public static void UpdateStudent(Student student)
+    {
+        using (var dbContext = new StudentDbContext())
+        {
+            var selected_student = dbContext.Students.FirstOrDefault<Student>(s => s.Id == student.Id);
+            if (selected_student != null)
+            {
+                selected_student.FirstName = student.FirstName;
+                selected_student.LastName = student.LastName;
+                selected_student.Address = student.Address;
+                dbContext.SaveChanges();
+            }
         }
     }
 }
