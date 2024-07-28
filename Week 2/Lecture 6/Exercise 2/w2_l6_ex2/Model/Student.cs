4567e67a7aaf486aeafa85f3522c53a872d3ec79
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-28 20:01:16 +1200

    Create Student Class

---
+++ Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Model/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace w2_l6_ex2.Model
+{
+    internal class Student
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
---
+++ Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace w2_l6_ex2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
