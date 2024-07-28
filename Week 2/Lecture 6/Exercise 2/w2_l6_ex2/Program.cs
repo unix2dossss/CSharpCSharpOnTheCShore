7900c5606c19d2c22e0fcf78f221bb4f8f39f356
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-28 20:42:47 +1200

    Create Student

--- Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Program.cs
+++ Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Program.cs
@@ -8,21 +8,27 @@ class Program
     static void Main(string[] args)
     {
         CreateStudent(new Student { Id = 1, FirstName = "Vandan", LastName = "Bhatt", Address = "Auckland" });
+
+        var students = GetStudents();
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Id: {student.Id}, FirstName: {student.FirstName}, LastName: {student.LastName}, Address: {student.Address}");
+        }
     }
 
     public static void CreateStudent(Student student)
     {
         using (var dbContext = new StudentDbContext())
         {
-            var s = dbContext.Students.FirstOrDefault<Student>(s => student.Id == s.Id);
-            if (s == null)
-            {
+            //var s = dbContext.Students.FirstOrDefault<Student>(s => student.Id == s.Id);
+            //if (s == null)
+            //{
                 dbContext.Students.Add(student);
                 dbContext.SaveChanges();
-            } else
-            {
-                Console.WriteLine($"Student with ID: {student.Id} already exists");
-            }
+            //} else
+            //{
+            //    Console.WriteLine($"Student with ID: {student.Id} already exists");
+            //}
         }
     }
 
