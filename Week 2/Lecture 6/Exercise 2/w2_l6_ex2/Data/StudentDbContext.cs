1035ac5777770e744d189a3cf65c988aa725ea5f
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-28 20:03:38 +1200

    Add Packages

---
+++ Week 2/Lecture 6/Exercise 2/w2_l6_ex2/Data/StudentDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w2_l6_ex2.Data
+{
+    internal class StudentDbContext
+    {
+    }
+}
