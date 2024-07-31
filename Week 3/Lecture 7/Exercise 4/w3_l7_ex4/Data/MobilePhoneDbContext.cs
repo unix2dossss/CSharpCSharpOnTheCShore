0605956b06192e12a9ff58c75515a6a92e222929
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-31 23:59:30 +1200

    Configure Service Container

---
+++ Week 3/Lecture 7/Exercise 4/w3_l7_ex4/Data/MobilePhoneDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace w3_l7_ex4.Data
+{
+    public class MobilePhoneDbContext : DbContext
+    {
+        
+    }
+}
--- Week 3/Lecture 7/Exercise 4/w3_l7_ex4/Program.cs
+++ Week 3/Lecture 7/Exercise 4/w3_l7_ex4/Program.cs
@@ -5,9 +5,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        // Creating the service container
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddControllers();
         builder.Services.AddAuthorization();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,25 +29,7 @@ public class Program
 
         app.UseAuthorization();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
+        app.MapControllers();
 
         app.Run();
     }
