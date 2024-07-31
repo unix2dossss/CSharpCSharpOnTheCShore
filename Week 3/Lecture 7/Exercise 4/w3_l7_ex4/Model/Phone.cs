6ab70fe5c17d0e8a14c19aa387d22f0d64c2b0eb
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-31 23:42:00 +1200

    Create Phone Model

---
+++ Week 3/Lecture 7/Exercise 4/w3_l7_ex4/Model/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace w3_l7_ex4.Model
+{
+    public class Phone
+    {
+        [Key]
+        public int PhoneId { get; set; }
+        [Required]
+        public string BrandName { get; set; }
+        [Required]
+        public string ModelName { get; set; }
+        [Required]
+        public float Price { get; set; }
+    }
+}
