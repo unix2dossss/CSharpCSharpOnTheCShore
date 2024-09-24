10a317676af92fde1539362beb3cabbc741ed500
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-09-24 17:33:38 +1200

    Complete JS Overview Exercise

---
+++ Week 8/b.js
@@ -0,0 +1,4 @@
+const myElem = document.getElementById("pickThis");
+myElem.addEventListener("click", () => {myElem.style.display = "none"});
+const myElem2 = document.getElementById("fstHead");
+myElem2.addEventListener("click", () => {myElem.style.display = "block"});
