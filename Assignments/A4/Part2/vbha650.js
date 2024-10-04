536d4074bee29ba7f1d4f05d21fffaa356172085
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-10-04 15:29:41 +1200

    Init Values

--- Assignments/A4/Part2/vbha650.js
+++ Assignments/A4/Part2/vbha650.js
@@ -9,4 +9,50 @@ async function getLogs() {
     }
 }
 
-getLogs();
+let dates = [];
+let visits = [];
+let uniqueVisits = [];
+let noOfLogPoints;
+let maxVisits;
+let minVisits;
+let height;
+let width;
+
+
+async function initValues() {
+    const app_logs = await getLogs();
+    console.log(app_logs);
+
+    app_logs.forEach(item => {
+        dates.push(item.date);
+        visits.push(item.visits);
+        uniqueVisits.push(item.uniqueVisits);
+    });
+
+    const allVisits = [...visits, ...uniqueVisits];
+
+    maxVisits = Math.max(...allVisits);
+    minVisits = Math.min(...allVisits);
+
+    height = maxVisits - minVisits;
+    width = height * 3.56;
+
+    noOfLogPoints = app_logs.length;
+    
+}
+
+async function generateGraph() {
+    await initValues();
+    console.log(`Number of log points: ${noOfLogPoints}`);
+    console.log(`Max Visits: ${maxVisits}`);
+    console.log(`Min Visits: ${minVisits}`);
+    console.log(`Height: ${height}`);
+    console.log(`Width: ${width}`);
+
+    for (let i = 0; i < noOfLogPoints; i++) {
+        console.log(`Index: ${i}`);
+    }
+}
+
+
+generateGraph();
